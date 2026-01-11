d6057cb7ffb49e06a65199465f293aff822b9183
Azizullah <azizullahcodes@gmail.com>
2026-01-11 21:45:11 +0500

    swapping images

---
+++ swappingImages/app.js
@@ -0,0 +1,12 @@
+let img = document.getElementById('img');
+let heading = document.getElementById('heading');
+
+function iqbal(){
+    img.setAttribute('src',"./assets/iqbal.jfif");
+    heading.innerHTML = 'I am Allama Iqbal';
+}
+
+function quaid(){
+    img.src="./assets/quaid.jfif";
+    heading.innerHTML = 'I am Quaid-E-Azam'
+}
